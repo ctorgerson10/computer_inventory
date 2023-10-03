93b5dec4ae086edeb935f560d94a720e43add9c0
CJ Torgerson <ctorgerson10@gmail.com>
2023-10-03 17:13:20 -0600

    added load and dump functions

--- main.py
+++ main.py
@@ -3,14 +3,24 @@ import json
 
 from models.menu import Menu
 from models.menuoption import MenuOption
+from models.computer import Computer
 from models.utilities import clear
 
 # regex pattern for IPv4 address
 ip_address_pattern = re.compile(r"(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9]["
                                 r"0-9]?)){3}")
 
+computers = []
 
 # Menu and function definitions
+def load_data(filename):
+    with open(filename, 'r+') as data:
+        raw: dict = json.load(data)
+    return raw
+
+def save_data(obj, filename):
+    with open(filename, 'w') as data:
+        json.dump(obj, filename)
 
 
 def add_computer():
@@ -21,11 +31,10 @@ def add_computer():
 
 
 def view_computers():
-    clear()
-    print("this will do something eventually")
-    input("press enter to return to the menu")
+    if computers is not None:
+        print(computers)
+    input("press enter to continue")
     main_menu.display()
-
 
 def edit_computer():
     clear()
@@ -42,4 +51,5 @@ main_menu = Menu("Main Menu", [
 
 
 if __name__ == "__main__":
+    computers = load_data("computer_inventory.json")
     main_menu.display()
---
+++ models/computer.py
@@ -0,0 +1,38 @@
+class Computer:
+
+    def __init__(self, ip: str, mac_address: str, os: str, status: str = "Active", name: str = None):
+        self.ip = ip
+        self.mac_address = mac_address
+        self.os = os
+        self.status = status
+        self.name = name if name is not None else f"Computer {self.ip}"
+
+    def __repr__(self):
+        return self.name if self.name != "Computer" else f"{self.name} {self.ip}"
+
+    def __str__(self):
+        return self.name if self.name != "Computer" else f"{self.name} {self.ip}"
+
+    def set_ip(self, ip):
+        self.ip = ip
+
+    def set_mac_address(self, mac):
+        self.mac_address = mac
+
+    def set_os(self, os):
+        self.os = os
+
+    def set_status(self, status: str):
+        statuses = ["Active", "Retired", "Missing"]
+        if status.title() in statuses:
+            self.status = status
+        else:
+            print(f"Invalid status, enter one of: {statuses}")
+
+    def display(self):
+        print(f"""
+        {self.name}
+        mac: {self.mac_address}
+        os: {self.os}
+        status: {self.status}
+        """)
